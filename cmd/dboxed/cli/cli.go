edc8425f4ec126bfc3bc67a8132e683565084bf8
Alexander Block <ablock84@gmail.com>
2025-09-22 09:57:07 +0200

    Introduce cli package and embed dboxed-volume

---
+++ cmd/dboxed/cli/cli.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/dboxed/dboxed/cmd/dboxed/commands"
+	"github.com/dboxed/dboxed/cmd/dboxed/flags"
+	"github.com/dboxed/dboxed/pkg/types"
+)
+
+type Cli struct {
+	flags.GlobalFlags
+
+	Run     commands.RunCmd     `cmd:"" help:"Download, unpack and run a box"`
+	Systemd commands.SystemdCmd `cmd:"" help:"Sub commands to control dboxed systemd integration"`
+	Runc    commands.RuncCmd    `cmd:"" help:"Run runc for a box"`
+
+	RunBoxInSandbox commands.RunBoxInSandbox `cmd:"" hidden:""`
+}
+
+func Execute() {
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	slog.SetDefault(slog.New(handler))
+
+	cli := &Cli{}
+
+	ctx := kong.Parse(cli,
+		kong.Name("dboxed"),
+		kong.Description("A simple container orchestrator."),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+			Summary: true,
+		}),
+		kong.Vars{
+			"default_infra_image": types.GetDefaultInfraImage(),
+		})
+
+	err := ctx.Run(&cli.GlobalFlags)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- cmd/dboxed/main.go
+++ cmd/dboxed/main.go
@@ -1,53 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log/slog"
 	"os"
 
-	"github.com/alecthomas/kong"
-	"github.com/dboxed/dboxed/cmd/dboxed/commands"
-	"github.com/dboxed/dboxed/cmd/dboxed/flags"
-	"github.com/dboxed/dboxed/pkg/types"
+	dboxed_volume_cli "github.com/dboxed/dboxed-volume/cmd/dboxed-volume/cli"
+	"github.com/dboxed/dboxed/cmd/dboxed/cli"
+
 	versionpkg "github.com/dboxed/dboxed/pkg/version"
 )
-
-type Cli struct {
-	flags.GlobalFlags
-
-	Run     commands.RunCmd     `cmd:"" help:"Download, unpack and run a box"`
-	Systemd commands.SystemdCmd `cmd:"" help:"Sub commands to control dboxed systemd integration"`
-	Runc    commands.RuncCmd    `cmd:"" help:"Run runc for a box"`
-
-	RunBoxInSandbox commands.RunBoxInSandbox `cmd:"" hidden:""`
-}
-
-func Execute() {
-	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	})
-	slog.SetDefault(slog.New(handler))
-
-	cli := &Cli{}
-
-	ctx := kong.Parse(cli,
-		kong.Name("dboxed"),
-		kong.Description("A simple container orchestrator."),
-		kong.UsageOnError(),
-		kong.ConfigureHelp(kong.HelpOptions{
-			Compact: true,
-			Summary: true,
-		}),
-		kong.Vars{
-			"default_infra_image": types.GetDefaultInfraImage(),
-		})
-
-	err := ctx.Run(&cli.GlobalFlags)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-}
 
 // set via ldflags
 var version = ""
@@ -59,5 +19,9 @@ func main() {
 		versionpkg.Version = version
 	}
 
-	Execute()
+	if os.Args[0] == "dboxed-volume" {
+		dboxed_volume_cli.Execute()
+	} else {
+		cli.Execute()
+	}
 }
