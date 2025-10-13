0ba096f3b13f17e11dbdfec38f056359d4d35283
Alexander Block <ablock84@gmail.com>
2025-10-13 19:51:50 +0200

    Faster infra-image caching

--- pkg/runner/sandbox/infra-image-pull.go
+++ pkg/runner/sandbox/infra-image-pull.go
@@ -5,6 +5,7 @@ package sandbox
 import (
 	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -12,9 +13,9 @@ import (
 
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
-	"github.com/google/go-containerregistry/pkg/v1/cache"
 	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/klauspost/compress/gzip"
 	"github.com/moby/go-archive"
 )
 
@@ -29,14 +30,19 @@ func (rn *Sandbox) pullInfraImage(ctx context.Context) error {
 }
 
 func (rn *Sandbox) pullImage(ctx context.Context, imageRef string, configPath, rootfs string) error {
-	imageCache := cache.NewFilesystemCache(filepath.Join(rn.HostWorkDir, "image-cache"))
+	imageCacheDir := filepath.Join(rn.HostWorkDir, "image-cache")
 
 	ref, err := name.ParseReference(imageRef)
 	if err != nil {
 		return err
 	}
 
-	slog.InfoContext(ctx, "pulling image", slog.Any("imageRef", ref.String()))
+	err = os.MkdirAll(imageCacheDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	slog.InfoContext(ctx, "retrieving image metadata", slog.Any("imageRef", ref.String()))
 
 	platform := v1.Platform{
 		Architecture: runtime.GOARCH,
@@ -47,11 +53,58 @@ func (rn *Sandbox) pullImage(ctx context.Context, imageRef string, configPath, r
 		return err
 	}
 
-	image = cache.Image(image, imageCache)
+	digest, err := image.Digest()
+	if err != nil {
+		return err
+	}
 
 	imageConfig, err := image.RawConfigFile()
 	if err != nil {
 		return err
+	}
+
+	cacheFile := filepath.Join(imageCacheDir, digest.Hex+".tar.gz")
+	if _, err := os.Stat(cacheFile); err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+		r := mutate.Extract(image)
+		defer r.Close()
+
+		slog.InfoContext(ctx, "pulling image", slog.Any("imageRef", ref.String()))
+
+		w, err := os.Create(cacheFile + ".tmp")
+		if err != nil {
+			return err
+		}
+		defer w.Close()
+
+		w2 := gzip.NewWriter(w)
+		if err != nil {
+			return err
+		}
+		defer w2.Close()
+
+		_, err = io.Copy(w2, r)
+		if err != nil {
+			return err
+		}
+		err = w2.Close()
+		if err != nil {
+			return err
+		}
+		err = w.Close()
+		if err != nil {
+			return err
+		}
+		err = r.Close()
+		if err != nil {
+			return err
+		}
+		err = os.Rename(cacheFile+".tmp", cacheFile)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = os.MkdirAll(filepath.Dir(configPath), 0700)
@@ -63,10 +116,21 @@ func (rn *Sandbox) pullImage(ctx context.Context, imageRef string, configPath, r
 		return err
 	}
 
-	r := mutate.Extract(image)
+	slog.InfoContext(ctx, "extracting image", slog.Any("imageRef", ref.String()))
+
+	r, err := os.Open(cacheFile)
+	if err != nil {
+		return err
+	}
 	defer r.Close()
 
-	err = archive.Untar(r, rootfs, &archive.TarOptions{})
+	r2, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer r2.Close()
+
+	err = archive.Untar(r2, rootfs, &archive.TarOptions{})
 	if err != nil {
 		return err
 	}
