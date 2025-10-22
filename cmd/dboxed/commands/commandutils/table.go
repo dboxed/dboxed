4f7df4e76e27a6d9505bdc6e6e3979f5c4f53611
Alexander Block <ablock84@gmail.com>
2025-10-22 15:58:53 +0200

    Beatiful tables thanks to charmbracelet

--- cmd/dboxed/commands/commandutils/table.go
+++ cmd/dboxed/commands/commandutils/table.go
@@ -1,10 +1,12 @@
 package commandutils
 
 import (
+	"fmt"
 	"io"
 	"reflect"
 
-	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 )
 
 type TableItem interface {
@@ -13,31 +15,38 @@ type TableItem interface {
 func FormatTable[T TableItem](l []T) string {
 	t := reflect.TypeFor[T]()
 
-	var header table.Row
+	// Extract headers
+	var headers []string
 	for i := range t.NumField() {
 		f := t.Field(i)
 		col := f.Tag.Get("col")
-		header = append(header, col)
+		headers = append(headers, col)
 	}
 
-	tw := table.NewWriter()
-	tw.AppendHeader(header)
-	tw.AppendSeparator()
-
+	// Extract rows
+	var rows [][]string
 	for _, e := range l {
 		v := reflect.Indirect(reflect.ValueOf(e))
-		var row table.Row
+		var row []string
 		for i := range t.NumField() {
 			f := v.Field(i)
-			//s, err := fmt.Print(f.Interface())
-			//if err != nil {
-			//	panic(err)
-			//}
-			row = append(row, f.Interface())
+			row = append(row, fmt.Sprintf("%v", f.Interface()))
 		}
-
-		tw.AppendRow(row)
+		rows = append(rows, row)
 	}
+
+	// Create lipgloss table with styling
+	tw := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("240"))).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			// Regular cell style
+			return lipgloss.NewStyle().
+				Foreground(lipgloss.Color("15")).
+				Padding(0, 1)
+		}).
+		Headers(headers...).
+		Rows(rows...)
 
 	return tw.Render()
 }
