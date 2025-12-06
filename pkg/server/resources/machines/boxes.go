8b5ee94e15d3a52bba93e975e9c9386009dbcbff
Alexander Block <ablock84@gmail.com>
2025-12-06 09:01:03 +0100

    Don't immediately delete machine box tokens

--- pkg/server/resources/machines/boxes.go
+++ pkg/server/resources/machines/boxes.go
@@ -3,6 +3,8 @@ package machines
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/dboxed/dboxed/pkg/server/auth_middleware"
@@ -11,6 +13,7 @@ import (
 	"github.com/dboxed/dboxed/pkg/server/huma_utils"
 	"github.com/dboxed/dboxed/pkg/server/models"
 	"github.com/dboxed/dboxed/pkg/server/resources/tokens"
+	"github.com/dboxed/dboxed/pkg/util"
 )
 
 func (s *MachinesServer) restListBoxes(c context.Context, i *huma_utils.IdByPath) (*huma_utils.List[models.Box], error) {
@@ -113,12 +116,8 @@ func (s *MachinesServer) restRemoveBox(c context.Context, i *restRemoveBoxInput)
 		return nil, err
 	}
 
-	tokenName := s.buildBoxTokenName(machine.ID, box.ID)
-	err = querier2.DeleteOneByFields[dmodel.Token](q, map[string]any{
-		"workspace_id": w.ID,
-		"name":         tokenName,
-	})
-	if err != nil && !querier2.IsSqlNotFoundError(err) {
+	err = s.invalidateBoxTokens(c, machine.ID, box.ID)
+	if err != nil {
 		return nil, err
 	}
 
@@ -158,15 +157,12 @@ func (s *MachinesServer) restCreateBoxToken(c context.Context, i *restCreateBoxT
 		return nil, huma.Error400BadRequest("box is not assigned to this machine")
 	}
 
-	tokenName := s.buildBoxTokenName(machine.ID, box.ID)
-	err = querier2.DeleteOneByFields[dmodel.Token](q, map[string]any{
-		"workspace_id": w.ID,
-		"name":         tokenName,
-	})
-	if err != nil && !querier2.IsSqlNotFoundError(err) {
+	err = s.invalidateBoxTokens(c, machine.ID, box.ID)
+	if err != nil {
 		return nil, err
 	}
 
+	tokenName := s.buildBoxTokenNamePrefix(machine.ID, box.ID) + util.RandomString(8)
 	token, err := tokens.CreateToken(c, w.ID, models.CreateToken{
 		Name:  tokenName,
 		Type:  dmodel.TokenTypeBox,
@@ -179,6 +175,39 @@ func (s *MachinesServer) restCreateBoxToken(c context.Context, i *restCreateBoxT
 	return huma_utils.NewJsonBody(*token), nil
 }
 
-func (s *MachinesServer) buildBoxTokenName(machineId string, boxId string) string {
-	return tokens.InternalTokenNamePrefix + fmt.Sprintf("machine_box_%s_%s", machineId, boxId)
+func (s *MachinesServer) listBoxTokens(ctx context.Context, machineId string, boxId string) ([]dmodel.Token, error) {
+	q := querier2.GetQuerier(ctx)
+	tokens, err := dmodel.ListTokensForBox(q, boxId)
+	if err != nil {
+		return nil, err
+	}
+	var ret []dmodel.Token
+	prefix := s.buildBoxTokenNamePrefix(machineId, boxId)
+	for _, t := range tokens {
+		if strings.HasPrefix(t.Name, prefix) {
+			ret = append(ret, t)
+		}
+	}
+	return ret, nil
+}
+
+func (s *MachinesServer) invalidateBoxTokens(ctx context.Context, machineId string, boxId string) error {
+	q := querier2.GetQuerier(ctx)
+	oldTokens, err := s.listBoxTokens(ctx, machineId, boxId)
+	if err != nil {
+		return err
+	}
+	for _, t := range oldTokens {
+		if t.ValidUntil == nil {
+			err = t.UpdateValidUntil(q, util.Ptr(time.Now().Add(5*time.Minute)))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *MachinesServer) buildBoxTokenNamePrefix(machineId string, boxId string) string {
+	return tokens.InternalTokenNamePrefix + fmt.Sprintf("machine_box_%s_%s_", machineId, boxId)
 }
