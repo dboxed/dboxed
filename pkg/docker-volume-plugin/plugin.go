df29b90abbcd2c8742b844dc7ae29beca77d6762
TheGeb <47231630+TheGeb@users.noreply.github.com>
2025-12-24 13:58:38 -0600

    rough draft of real plugin code

--- pkg/docker-volume-plugin/plugin.go
+++ pkg/docker-volume-plugin/plugin.go
@@ -1,21 +1,17 @@
 package docker_volume_plugin
 
 import (
-	// "fmt"
 	"context"
-	"log"
 	"errors"
-	// "os"
-	// "path/filepath"
+	"path/filepath"
+	"time"
 
-	// "github.com/dboxed/dboxed/cmd/dboxed/commands/commandutils"
-	volume_mount "github.com/dboxed/dboxed/cmd/dboxed/commands/volume-mount"
-	"github.com/dboxed/dboxed/cmd/dboxed/flags"
+	"github.com/dboxed/dboxed/pkg/baseclient"
 	"github.com/dboxed/dboxed/pkg/clients"
-	"github.com/dboxed/dboxed/pkg/server/models"
+	plugin_config "github.com/dboxed/dboxed/pkg/docker-volume-plugin/config"
+	"github.com/dboxed/dboxed/pkg/services"
+	"github.com/dboxed/dboxed/pkg/volume/volume_serve"
 	volume_helper "github.com/docker/go-plugins-helpers/volume"
-	// volume_plugin "github.com/dboxed/dboxed/pkg/docker-volume-plugin"
-	// plugin_config "github.com/dboxed/dboxed/pkg/docker-volume-plugin/config"
 )
 
 const pluginRoot = "/var/lib/dboxed/volumes"
@@ -23,170 +19,156 @@ const pluginRoot = "/var/lib/dboxed/volumes"
 type Driver struct{}
 
 type driver_opts struct {
-}
-
-func GetGlobals() *flags.GlobalFlags {
-	// TODO load from env or config file?
-	return &flags.GlobalFlags{}
+	volumeName     string
+	volumeProvider string
+	fsSize         string
+	fsType         string
+	backupInterval string
 }
 
 func parseVolumeOptions(options map[string]string) driver_opts {
-	parsedOptions := driver_opts{}
-	return parsedOptions
+	return driver_opts{
+		volumeName:     options["volume_name"], // might not be needed, already a part of docker api request model?
+		volumeProvider: options["volume_provider"],
+		fsSize:         options["fs_size"],
+		fsType:         options["fs_type"],
+		backupInterval: options["backup_interval"],
+	}
+}
+
+func buildClient(ctx context.Context, config *plugin_config.Config) (*baseclient.Client, error) {
+	c, err := baseclient.New(nil, nil, false)
+	return c, err
 }
 
 func (d *Driver) Create(req *volume_helper.CreateRequest) error {
-	// req.Name
-	// req.Options
 
-	gf := GetGlobals()
+	// Attempt to get + mount the volume, will create if necessary
+
+	// backupInterval is only included on the initial Create request for plugin api, but would also be needed on mount.
+	// instead, should there be a config.json for default backupInterval, and allow per-volume override elsewhere?
+	// Options are environment, driver_opts, file-based overrides, or maybe something from dboxed UI?
 
 	ctx := context.Background()
+	opts := parseVolumeOptions(req.Options)
+	config, err := plugin_config.LoadConfig("")
 
-	c, err := gf.BuildClient(ctx)
+	c, err := buildClient(ctx, config)
 	if err != nil {
 		return err
 	}
 
 	c2 := clients.VolumesClient{Client: c}
+
 	v, err := c2.GetVolumeByName(ctx, req.Name)
-
-	if v != nil {
-		//TODO: check if volume is already mounted elsewhere... if mounted by self, return and give no error?
-		return errors.New("volume already exists")
-	}
-
-	_, err = c2.CreateVolume(ctx, models.CreateVolume{
-		Name: req.Name,
-		// TODO other params?
-	})
-
-	return err
-
-	// v.Attachment
-	// v.MountStatus
-	// v.VolumeProvider.Name
-
-	// ct := commandutils.NewClientTool(c)
-
-	// TODO check if dboxed volume already exists before creating and validate
-	// cmd := &volume.CreateCmd{
-	// 	Name:           req.Name,
-	// 	VolumeProvider: "rustic-test",
-	// 	FsType:         "btrfs",
-	// 	// FsSize:         "",
-	// }
-
-	//TODO: write metadata for later use?
-
-	// return cmd.Run(gf)
-
-	// volPath := filepath.Join(pluginRoot, req.Name)
-	// log.Printf("Create volume: %s", volPath)
-	// return os.MkdirAll(volPath, 0755)
-}
-
-func (d *Driver) Mount(req *volume_helper.MountRequest) (*volume_helper.MountResponse, error) {
-
-	// TODO: run serve in background?
-	// args := &flags.VolumeServeArgs{
-	// 	Volume:         "test",
-	// 	BackupInterval: "30m",
-	// }
-
-	// cmd := &volume_mount.ServeCmd{
-	// 	args,
-	// }
-
-	// TODO check if dboxed volume-mount already exists before creating?
-	cmd := &volume_mount.CreateCmd{
-		Volume: req.Name,
-	}
-
-	gf := GetGlobals()
-
-	err := cmd.Run(gf)
-	if err != nil {
-		return nil, err
-	}
-
-	var res *volume_helper.MountResponse
-	// v, err := d.d.Get(req.Name)
-	// if err != nil {
-	// 	return &volume_helper.MountResponse{}, err
-	// }
-	// pth, err := v.Mount(req.ID)
-	// if err != nil {
-	// 	return &volume_helper.MountResponse{}, err
-	// }
-	// res.Mountpoint = pth
-	return res, nil
-
-	// volPath := filepath.Join(pluginRoot, req.Name)
-
-	// // Write a hello.txt file
-	// helloFile := filepath.Join(volPath, "hello.txt")
-	// err := os.WriteFile(helloFile, []byte("Hello, world!\n"), 0644)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to write hello.txt: %w", err)
-	// }
-
-	// log.Printf("Mount volume: %s -> %s", req.Name, volPath)
-	// return &volume_helper.MountResponse{Mountpoint: volPath}, nil
-}
-
-func (d *Driver) Unmount(req *volume_helper.UnmountRequest) error {
-	// TODO check if dboxed volume already exists before creating
-	cmd := &volume_mount.ReleaseCmd{
-		Volume: req.Name,
-	}
-
-	gf := GetGlobals()
-
-	err := cmd.Run(gf)
 	if err != nil {
 		return err
 	}
 
-	// v, err := d.d.Get(req.Name)
-	// if err != nil {
-	// 	return err
-	// }
-	// if err := v.Unmount(req.ID); err != nil {
-	// 	return err
-	// }
+	if v != nil {
+		// if volume already matches requested options, return success? Or start serving immediately?
+		// If volume doesn't match, throw error?
+		return errors.New("volume already exists")
+	}
+
+	s := &services.VolumesService{Client: c}
+	err = s.CreateVolume(&services.CreateVolumeCmdOpts{
+		Name:           req.Name,
+		VolumeProvider: opts.volumeProvider,
+		FsType:         opts.fsType,
+		FsSize:         opts.fsSize,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = s.RunServeVolumeCmd(ctx, pluginRoot, services.RunServeVolumeCmdOpts{
+		Volume:  req.Name,
+		Create:  false, //seems to refer to volumeState, not the volume itself
+		Mount:   true,
+		Serve:   false,
+		Release: false,
+	})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (d *Driver) Mount(req *volume_helper.MountRequest) (*volume_helper.MountResponse, error) {
+
+	// serve the volume
+
+	//TODO: How to handle if volume isn't already created/mounted?
+	//TODO: do I need to do anything with req.ID?
+	ctx := context.Background()
+	volumeState, err := volume_serve.LoadVolumeState(filepath.Join(pluginRoot, req.Name))
+	if err != nil {
+		return nil, err
+	}
+
+	if volumeState != nil {
+		//TODO: update volumeState with new options if necessary?
+	}
+
+	c, err := buildClient(ctx, plugin_config.GetConfig(ctx))
+	if err != nil {
+		return nil, err
+	}
+
+	s := &services.VolumesService{Client: c}
+
+	// TODO: figure out how to handle backupInterval
+	// TODO: need to run serve in background? how to hold onto a reference of this process for Unmount to stop later?
+	err = s.RunServeVolumeCmd(ctx, pluginRoot, services.RunServeVolumeCmdOpts{
+		Volume: req.Name,
+		// BackupInterval:    &cmd.BackupInterval,
+		// WebdavProxyListen: &cmd.WebdavProxyListen,
+		Create:  true,
+		Mount:   true,
+		Serve:   true,
+		Release: true,
+	})
+
+	return &volume_helper.MountResponse{Mountpoint: filepath.Join(pluginRoot, req.Name)}, nil
+}
+
+func (d *Driver) Unmount(req *volume_helper.UnmountRequest) error {
+	//TODO: simply send stop request to volume serve?
 	return nil
-
-	// volPath := filepath.Join(pluginRoot, req.Name)
-	// helloFile := filepath.Join(volPath, "hello.txt")
-
-	// // Simulate cleanup
-	// if err := os.Remove(helloFile); err != nil && !os.IsNotExist(err) {
-	// 	return fmt.Errorf("unmount cleanup error: %w", err)
-	// }
-
-	// log.Printf("Unmount volume: %s (removed hello.txt)", req.Name)
-	// return nil
 }
 
 func (d *Driver) Remove(req *volume_helper.RemoveRequest) error {
-	// TODO is anything needed here? Might need to unmount?
+	// get volumeState and remove mount, do nothing if volumeState is not found for some reason?
 
-	// v, err := d.d.Get(req.Name)
-	// if err != nil {
-	// 	return err
-	// }
-	// if err := d.d.Remove(v); err != nil {
-	// 	return err
-	// }
+	volumeState, err := volume_serve.LoadVolumeState(filepath.Join(pluginRoot, req.Name))
+	if err != nil {
+		return err
+	}
 
-	// volPath := filepath.Join(pluginRoot, req.Name)
+	if volumeState == nil {
+		return nil
+	}
 
-	// log.Printf("Remove volume: %s", volPath)
-	// err := os.RemoveAll(volPath)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to remove volume: %w", err)
-	// }
+	ctx := context.Background()
+	c, err := buildClient(ctx, plugin_config.GetConfig(ctx))
+	if err != nil {
+		return nil
+	}
+	s := &services.VolumesService{Client: c}
+
+	err = s.RunServeVolumeCmd(ctx, pluginRoot, services.RunServeVolumeCmdOpts{
+		Volume: req.Name,
+		// WebdavProxyListen: &cmd.WebdavProxyListen,
+		Create:  false,
+		Mount:   false,
+		Serve:   false,
+		Release: true,
+	})
+	if err != nil {
+		return nil
+	}
 
 	return nil
 }
@@ -194,71 +176,49 @@ func (d *Driver) Remove(req *volume_helper.RemoveRequest) error {
 func (d *Driver) Get(req *volume_helper.GetRequest) (*volume_helper.GetResponse, error) {
 	//TODO: check locally stored metadata for volume info?
 
-	var res *volume_helper.GetResponse
-	// v, err := d.d.Get(req.Name)
-	// if err != nil {
-	// 	return &volume_helper.GetResponse{}, err
-	// }
+	// ctx := context.Background()
+	mountPoint := filepath.Join(pluginRoot, req.Name)
 
-	var mountPoint string
-	var status map[string]interface{}
+	volumeState, err := volume_serve.LoadVolumeState(filepath.Join(pluginRoot, req.Name))
+	if err != nil {
+		return nil, err
+	}
+
+	var res *volume_helper.GetResponse
+
+	var status map[string]interface{} //FIXME: What are available statuses?
 
 	res.Volume = &volume_helper.Volume{
 		Name:       req.Name,
 		Mountpoint: mountPoint,
-		CreatedAt:  "", // TODO fill created at
+		CreatedAt:  volumeState.Volume.CreatedAt.Format(time.RFC3339), //TODO: verify format
 		Status:     status,
 	}
 	return &volume_helper.GetResponse{}, nil
-
-	// volPath := filepath.Join(pluginRoot, req.Name)
-
-	// Confirm it exists
-	// info, err := os.Stat(volPath)
-	// if os.IsNotExist(err) {
-	// 	return nil, fmt.Errorf("volume %s not found", req.Name)
-	// } else if err != nil || !info.IsDir() {
-	// 	return nil, fmt.Errorf("invalid volume path: %w", err)
-	// }
-
-	// return &volume_helper.GetResponse{
-	// 	Volume: &volume_helper.Volume{
-	// 		Name:       req.Name,
-	// 		Mountpoint: volPath,
-	// 	},
-	// }, nil
 }
 
 func (d *Driver) List() (*volume_helper.ListResponse, error) {
-	//TODO: check locally stored metadata for volume info?
+	volumeStates, err := volume_serve.ListVolumeState(pluginRoot)
+	if err != nil {
+		return nil, err
+	}
 
 	var res *volume_helper.ListResponse
+	volumes := make([]*volume_helper.Volume, len(volumeStates))
 
-	ls := []volume_helper.Volume{} // TODO get actual list
+	for _, vs := range volumeStates {
+		var status map[string]interface{} //FIXME: What are available statuses?
 
-	vols := make([]*volume_helper.Volume, len(ls))
+		volumes = append(volumes, &volume_helper.Volume{
+			Name:       vs.Volume.Name,
+			Mountpoint: filepath.Join(pluginRoot, vs.Volume.Name),
+			CreatedAt:  vs.Volume.CreatedAt.Format(time.RFC3339), //TODO: verify format
+			Status:     status,
+		})
+	}
 
-	res.Volumes = vols
-
+	res.Volumes = volumes
 	return res, nil
-
-	// entries, err := os.ReadDir(pluginRoot)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to list volumes: %w", err)
-	// }
-
-	// var volumes []*volume_helper.Volume
-	// for _, entry := range entries {
-	// 	if entry.IsDir() {
-	// 		volPath := filepath.Join(pluginRoot, entry.Name())
-	// 		volumes = append(volumes, &volume_helper.Volume{
-	// 			Name:       entry.Name(),
-	// 			Mountpoint: volPath,
-	// 		})
-	// 	}
-	// }
-
-	// return &volume_helper.ListResponse{Volumes: volumes}, nil
 }
 
 func (d *Driver) Capabilities() *volume_helper.CapabilitiesResponse {
@@ -270,29 +230,15 @@ func (d *Driver) Capabilities() *volume_helper.CapabilitiesResponse {
 }
 
 func (d *Driver) Path(req *volume_helper.PathRequest) (*volume_helper.PathResponse, error) {
-	//TODO: check locally stored metadata for volume info?
+	//TODO: check volume state and return mount path
 
-	var res *volume_helper.PathResponse
-	// v, err := d.d.Get(req.Name)
-	// if err != nil {
-	// 	return &volume_helper.PathResponse{}, err
-	// }
-	// res.Mountpoint = v.Path()
-	return res, nil
-
-	// volPath := filepath.Join(pluginRoot, req.Name)
-	// return &volume_helper.PathResponse{Mountpoint: volPath}, nil
-}
-
-func main() {
-	// if needed, do migrations if an older metadata structure is found?
-
-	Driver := &Driver{}
-	h := volume_helper.NewHandler(Driver)
-	log.Print("Starting plugin ...")
-
-	//TODO customize GID?
-	if err := h.ServeUnix("dboxed", 0); err != nil {
-		log.Fatalf("plugin serve error: %v", err)
+	// ctx := context.Background()
+	_, err := volume_serve.LoadVolumeState(filepath.Join(pluginRoot, req.Name))
+	if err != nil {
+		return nil, err
 	}
+
+	// volumeState.Volume.Attachment?
+
+	return &volume_helper.PathResponse{Mountpoint: filepath.Join(pluginRoot, req.Name)}, nil
 }
